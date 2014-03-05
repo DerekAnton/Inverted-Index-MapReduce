e6ad140c3c8c24600ee418d73c4094265b7eb046
Danton <danton5276@gmail.com>
2014-03-04 21:10:54 -0500

    Added basics to handle command line parameters (this stores the amount of requested threads, and loads a file object in for the requested filenames)

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,44 @@
 
+import java.util.concurrent.Semaphore;
+import java.io.File;
+
 public class Index 
 {
-
+	public static void main(String[] args)
+	{
+		int requestedThreads = 0;
+		File fileArray[] = null;
+		
+		try
+		{
+			requestedThreads = Integer.parseInt(args[0]);
+			for(int counter = 0; counter <args.length; counter++)
+			{
+				fileArray[counter] = new File(args[counter]);
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("The parameters passed were not of the form");
+			System.out.println("Index [n] [file1] [file2] ... [filen]");
+			System.out.println("Please re-run the program and try again");
+		}
+	}
 }
+
+/* INPUT
+ * java Index [n] [file1] [file2] ... [filen]
+ * 		With n Reducer threads & k Mapper threads
+ * 
+ * OUTPUT
+ * word1 (file@line,line... file@line,line,...)...
+ * word2 (file@line,line... file@line,line,...)...
+ * 
+ * TODOs:
+ *  1.) Each map thread will be assigned to the next unique txt file
+ *  		I'm going to have to create requestedThreads number of threads and give each one 
+ *  		the task to read in the next file passed as a parameter in args[] 
+ * 	2.) Each map thread will hash each word and compute an int from 1-n (n being the number of threads created?)
+ * 	
+ * 
+ */
